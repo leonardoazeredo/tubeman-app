ac15783672b38a328e71007e0538deb2ec1cadfa
Leonardo <leonardo.azeredo@live.com>
2025-02-19 13:36:44 -0300

    added prisma and user model to schema

---
+++ prisma/migrations/20250218211549_added_user_schema/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(6) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_name_idx" ON "User"("name");
+
+-- CreateIndex
+CREATE INDEX "User_email_idx" ON "User"("email");
