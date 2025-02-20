800d0925e82c7fe3388332a3e913056183d74245
Leonardo <leonardo.azeredo@live.com>
2025-02-19 23:58:00 -0300

    refactor: Improve auth error handling.
    - Refactor the authentication flow by improving the error handling during the credentials validation process.

--- auth.ts
+++ auth.ts
@@ -3,39 +3,39 @@ import Credentials from "next-auth/providers/credentials"
 import { signInSchema } from "@/utils/zodSchemas";
 import { validatePassword, validateUser } from "@/utils/utilities";
 import { DbUser } from "@/types/db";
- 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
-        async authorize(credentials):Promise<DbUser | null> {
+      async authorize(credentials): Promise<DbUser | null> {
 
-          const parsedCredentials = signInSchema.safeParse(credentials);
-  
-          if (!parsedCredentials.success) {
-            //  This returns an object with `formErrors` and `fieldErrors`.
-            //  We'll use `fieldErrors` for field-specific errors.
-            const errors = parsedCredentials.error.flatten().fieldErrors;
-            // Convert fieldErrors to an array of objects
-            const errorArray = Object.entries(errors).flatMap(([field, messages]) =>
-                  messages.map(message => ({ field, message }))
-            );
-  
-              throw new CredentialsSignin(
-                JSON.stringify({ type: "validation", errors: errorArray })
-              );
-          }
-  
-          const { email, password } = parsedCredentials.data;
-          const user: DbUser | null = await validateUser(email);
-  
-          if (!user || !(await validatePassword(password, user.password))) {
+        const parsedCredentials = signInSchema.safeParse(credentials);
 
-            return null;
-          }
+        if (!parsedCredentials.success) {
+          //  This returns an object with `formErrors` and `fieldErrors`.
+          //  We'll use `fieldErrors` for field-specific errors.
+          const errors = parsedCredentials.error.flatten().fieldErrors;
+          // Convert fieldErrors to an array of objects
+          const errorArray = Object.entries(errors).flatMap(([field, messages]) =>
+            messages.map(message => ({ field, message }))
+          );
 
-  
-          return user;
-        },
-      }),
+          throw new CredentialsSignin(
+            JSON.stringify({ type: "validation", errors: errorArray })
+          );
+        }
+
+        const { email, password } = parsedCredentials.data;
+        const user: DbUser | null = await validateUser(email);
+
+        if (!user || !(await validatePassword(password, user.password))) {
+
+          return null;
+        }
+
+
+        return user;
+      },
+    }),
   ],
 })
