d1d88d68c33502f6f60e512b8325fbf3599084fc
Leonardo <leonardo.azeredo@live.com>
2025-02-22 14:41:15 -0300

    refactor: Update app layout and welcome message
    - Update application title and description.
    
    - Replace the content of the page with a welcome message and a link to login.
    
    - Remove the body tag from the auth layout.

--- app/ui/layouts/auth-layout.tsx
+++ app/ui/layouts/auth-layout.tsx
@@ -7,18 +7,16 @@ interface AuthLayoutProps {
 
 const AuthLayout: React.FC<AuthLayoutProps> = ({ title, children }) => {
   return (
-    <body className="antialiased">
-      <main className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
-          <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              {title}
-            </h2>
-          </div>
-          {children}
+    <main className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            {title}
+          </h2>
         </div>
-      </main>
-    </body>
+        {children}
+      </div>
+    </main>
   );
 };
 
