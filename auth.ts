aa64d5183fab7faa111dd5b22bdbe9b414792672
Leonardo <leonardo.azeredo@live.com>
2025-02-22 15:36:23 -0300

    feat: Configure authentication
    - Configure authentication with routes, credentials, error handling, and authorization callbacks.

--- auth.ts
+++ auth.ts
@@ -1,14 +1,31 @@
-import NextAuth, { CredentialsSignin } from "next-auth"
-import Credentials from "next-auth/providers/credentials"
+import NextAuth, { CredentialsSignin } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import { signInSchema } from "@/utils/zodSchemas";
 import { validatePassword, validateUser } from "@/utils/utilities";
 import { DbUser } from "@/types/db";
 
+export const routes = [
+  // {
+  //   name: "Dashboard",
+  //   href: "/dashboard",
+  // },
+  {
+    name: "Collections",
+    href: "/collections",
+  },
+  {
+    name: "Scraper",
+    href: "/scraper",
+  },
+];
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  pages: {
+    signIn: "/login",
+  },
   providers: [
     Credentials({
       async authorize(credentials): Promise<DbUser | null> {
-
         const parsedCredentials = signInSchema.safeParse(credentials);
 
         if (!parsedCredentials.success) {
@@ -16,8 +33,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           //  We'll use `fieldErrors` for field-specific errors.
           const errors = parsedCredentials.error.flatten().fieldErrors;
           // Convert fieldErrors to an array of objects
-          const errorArray = Object.entries(errors).flatMap(([field, messages]) =>
-            messages.map(message => ({ field, message }))
+          const errorArray = Object.entries(errors).flatMap(
+            ([field, messages]) =>
+              messages.map((message) => ({ field, message }))
           );
 
           throw new CredentialsSignin(
@@ -29,7 +47,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         const user: DbUser | null = await validateUser(email);
 
         if (!user || !(await validatePassword(password, user.password))) {
-
           console.log("User failed to login");
           return null;
         }
@@ -39,4 +56,18 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
     }),
   ],
-})
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnProtectedRoute = routes.some((link) =>
+        nextUrl.pathname.startsWith(link.href)
+      );
+      // If the user is not logged in and on a protected route, deny access
+      if (!isLoggedIn && isOnProtectedRoute) {
+        return false;
+      }
+      // Otherwise, allow access
+      return true;
+    },
+  },
+});
